83e1529f51acb996ffb782857da66c7f04dd56bf
Sayim Riaz <sayimriaz16@gmail.com>
2024-10-21 12:41:41 +0000

    cronjob

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,9 +16,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
 
-  // useEffect(() => {
-  //   startBackgroundTasks();
-  // }, []);
+  useEffect(() => {
+    startBackgroundTasks();
+  }, []);
 
   return (
     <html lang="en">
